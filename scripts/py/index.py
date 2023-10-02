c2e844a710d65860e2f14d8d726b089f4c2b834c
butterflykoko <ktaketa@hotmail.com>
2023-10-02 16:38:41 -0700

    Deleted imported library

--- scripts/py/index.py
+++ scripts/py/index.py
@@ -1,11 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-x = np.array([2, 2, 4, 4, 4,  5, 6, 6])
-y = np.array([3, 3, 3, 4, 4, 5, 5, 5])
+x = [2, 2, 4, 4, 4, 5, 6, 6]
+y = [3, 3, 3, 4, 4, 5, 5, 5]
 
-slope, intercept, r, std_err = stats.linregress(x,y)
+slope, intercept, r, p, std_err = stats.linregress(x, y)
 
 def myfunc(x):
   return slope * x + intercept
