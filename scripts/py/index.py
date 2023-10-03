fda1a23d8737701fb8f62ff285aaf7070dec593b
butterflykoko <ktaketa@hotmail.com>
2023-10-02 17:55:22 -0700

    Deleted unused py file

--- scripts/py/index.py
+++ scripts/py/index.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 
-x = [2, 2, 4, 4, 4, 5, 6, 6]
-y = [3, 3, 3, 4, 4, 5, 5, 5]
+x = [0, 0.75, 1, 1.5, 2, 3, 3.5, 4]
+y = [0.67, 0.99, 1.15, 1.29, 1.21, 1.16, 1.19, 1.15]
 
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
+
 def myfunc(x):
-  return slope * x + intercept
+    return slope * x + intercept
+
+
+year = 5
+price = myfunc(year)
+txt = "The price for the apples per {year}: {}"
 
 model = list(map(myfunc, x))
 
 plt.scatter(x, y)
 plt.plot(x, model)
 plt.show()
+
+print(txt.format(round(price, 2)))
--- scripts/py/this.py
+++
@@ -1,5 +0,0 @@
-x = 0
-m = 2
-y = m * x + 0
-answer = "({}, {})"
-print(answer.format(x, y))
