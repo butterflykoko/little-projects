7c03acd0183ed8cfa6e8e8f3bec74340ca130717
butterflykoko <116604542+butterflykoko@users.noreply.github.com>
2023-10-08 18:35:20 -0700

    More changes

--- public/calander/calander.js
+++ public/calander/calander.js
@@ -49,71 +49,34 @@ let header = () => {
 
 header();
 
-let days = () => {
-  let m;
-  switch (new Date().getMonth()) {
-    case 0:
-      m = 31;
-      break;
-    case 1:
-      m = 28;
-      break;
-    case 2:
-      m = 31;
-      break;
-    case 3:
-      m = 30;
-      break;
-    case 4:
-      m = 31;
-      break;
-    case 5:
-      m = 30;
-      break;
-    case 6:
-      m = 31;
-      break;
-    case 7:
-      m = 31;
-      break;
-    case 8:
-      m = 30;
-      break;
-    case 9:
-      m = 31;
-      break;
-    case 10:
-      m = 30;
-      break;
-    case 11:
-      m = 31;
-  }
+const days = () => {
+  var day = document.getElementById("days").innerHTML;
+  let day1 = new Date(year, month, 1).getDay();
 
-  let num = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-  ];
+  let endDate = new Date(year, month + 1, 0).getDate();
+  let lastDay = new Date(year, month, endDate).getDay();
 
-  let numbers = () => {
-    let i = 0;
-    while (i < m) {
-      for (let j of num) {
-        j++;
-        return num;
-      }
+  let i = 1;
+
+  for (i <= endDate; i++; ) {
+    if (i === date.getDate()) {
+      const li = document.createElement("li");
+      const num = document.createTextNode(i);
+
+      li.appendChild(num);
+      day += document.getElementById("days").appendChild(li);
     }
-
-    return num;
-  };
-
-  numbers();
-
-  for (let i; i >= m; i++) {
-    const li = document.createElement("li");
-    let n = document.createTextNode(numbers());
-    li.appendChild(n);
-    document.getElementById("days").appendChild(li);
   }
+
+  for (let j = lastDay; j < 6; i++) {  
+    const li = document.createElement("li");
+    const num = document.createTextNode(j - lastDay);
+
+    li.appendChild(num);
+    let el = document.getElementById("days").appendChild(li);
+    day += el.classList.add("inactive");
+  }
+
 };
 
 days();
