810defadf2e8262baeb36fc2aa60157379a02438
butterflykoko <116604542+butterflykoko@users.noreply.github.com>
2023-10-11 14:44:54 -0700

    New file

--- public/word-generator/word.js
+++ public/word-generator/word.js
@@ -1,1 +1,11 @@
-"use strict";
+let showWords = () => {
+
+}
+
+// import { ValidationChain } from "express-validator";
+// import { body } from "express-validator";
+// app.post("/word", body("first-letter").custom(async value => {
+//   const onlyLetters = await 
+// })
+
+// );
